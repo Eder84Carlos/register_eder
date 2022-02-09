188074e1b970a6307d70bf54b8b3d98e2adae589
Eder Carvalho <91353220+Eder84Carlos@users.noreply.github.com>
2022-02-09 02:18:10 -0300

    Projeto em andamento

--- src/main/java/com/example/register_eder/resources/SellerResource.java
+++ src/main/java/com/example/register_eder/resources/SellerResource.java
@@ -1,20 +1,33 @@
 package com.example.register_eder.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.register_eder.entities.Seller;
+import com.example.register_eder.services.SellerService;
 
 @RestController
 @RequestMapping(value = "/Seller")
 public class SellerResource {
 	
+	@Autowired
+	private SellerService service;
+	
 	@GetMapping
-	public ResponseEntity<Seller> findAll(){
-		Seller u = new Seller(1L, "Eder Carlos");
-		return ResponseEntity.ok().body(u);
-
+	public ResponseEntity<List<Seller>> findAll(){
+		List<Seller> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Seller> findById(@PathVariable Long id){
+		Seller obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
---
+++ src/main/java/com/example/register_eder/services/SellerService.java
@@ -0,0 +1,27 @@
+package com.example.register_eder.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.register_eder.entities.Seller;
+import com.example.register_eder.repositories.SellerRepository;
+
+@Service
+public class SellerService {
+	
+	@Autowired
+	private SellerRepository repository;
+	
+	public List<Seller> findAll(){
+		return repository.findAll();
+	}
+	
+	public Seller findById(Long id) {
+		Optional<Seller> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
