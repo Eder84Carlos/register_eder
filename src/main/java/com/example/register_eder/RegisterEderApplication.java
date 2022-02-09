479556bcf2d5a3c7059d523517748985877dad03
Eder Carvalho <91353220+Eder84Carlos@users.noreply.github.com>
2022-02-08 23:30:23 -0300

    Projeto Criado

---
+++ src/main/java/com/example/register_eder/RegisterEderApplication.java
@@ -0,0 +1,13 @@
+package com.example.register_eder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterEderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterEderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/register_eder/RegisterEderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.register_eder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterEderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
